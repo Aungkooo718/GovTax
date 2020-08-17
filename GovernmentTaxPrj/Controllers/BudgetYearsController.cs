13a2df68cdb511400e34e79127c34405efc691c7
aungkooo510 <ooaungko2665@gmail.com>
2020-08-17 17:41:08 +0630

    Add GovTaxProject

---
+++ GovernmentTaxPrj/Controllers/BudgetYearsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GovernmentTaxPrj.Data;
+using GovernmentTaxPrj.Models;
+
+namespace GovernmentTaxPrj.Controllers
+{
+    public class BudgetYearsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BudgetYearsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BudgetYears
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BudgetYear.ToListAsync());
+        }
+
+        // GET: BudgetYears/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var budgetYear = await _context.BudgetYear
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (budgetYear == null)
+            {
+                return NotFound();
+            }
+
+            return View(budgetYear);
+        }
+
+        // GET: BudgetYears/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BudgetYears/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Code,StartDate,EndDate,Status")] BudgetYear budgetYear)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(budgetYear);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(budgetYear);
+        }
+
+        // GET: BudgetYears/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var budgetYear = await _context.BudgetYear.FindAsync(id);
+            if (budgetYear == null)
+            {
+                return NotFound();
+            }
+            return View(budgetYear);
+        }
+
+        // POST: BudgetYears/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Code,StartDate,EndDate,Status")] BudgetYear budgetYear)
+        {
+            if (id != budgetYear.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(budgetYear);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BudgetYearExists(budgetYear.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(budgetYear);
+        }
+
+        // GET: BudgetYears/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var budgetYear = await _context.BudgetYear
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (budgetYear == null)
+            {
+                return NotFound();
+            }
+
+            return View(budgetYear);
+        }
+
+        // POST: BudgetYears/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var budgetYear = await _context.BudgetYear.FindAsync(id);
+            _context.BudgetYear.Remove(budgetYear);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BudgetYearExists(long id)
+        {
+            return _context.BudgetYear.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GovernmentTaxPrj/Migrations/20200813105916_budgetyear.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovernmentTaxPrj.Migrations
+{
+    public partial class budgetyear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TinNumber",
+                table: "TaxPayer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TaxType",
+                table: "TaxPayer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nrc",
+                table: "TaxPayer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "TaxPayer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "TaxPayer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BudgetYear",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Code = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetYear", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BudgetYear");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TinNumber",
+                table: "TaxPayer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TaxType",
+                table: "TaxPayer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nrc",
+                table: "TaxPayer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "TaxPayer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "TaxPayer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
